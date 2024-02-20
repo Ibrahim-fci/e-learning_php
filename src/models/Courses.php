11693d960a6a13183c5472caa4196ab27cb632f2
dinanady <dinasaber63@gmail.com>
2024-02-20 06:20:25 +0200

    ....

--- src/models/Courses.php
+++ src/models/Courses.php
@@ -1,43 +1,64 @@
 <?php
-class Course{
-    private $connection;
-    public $id,$title,$img,$description,$instructor,$duration,$price,$category;
-    public function __construct()
+
+include_once __DIR__ . "/../models/DbConnector.php";
+
+class Course extends DbConnector
+{
+    public $title, $imageurl, $description, $price, $duration, $teacher_id, $category_id;
+    public function __construct($title, $imageurl, $description, $price, $duration, $teacher_id, $category_id)
     {
-        try{
-            $this->connection=new PDO("mysql:host=localhost;dbname=e_learning;charset=utf8","root","");
-        }catch(Exception $e){
-            $e->getMessage();
-        }
+        parent::__construct();
+
+        $this->title = $title;
+        $this->imageurl = $imageurl;
+        $this->description = $description;
+        $this->price = $price;
+        $this->duration = $duration;
+        $this->teacher_id = $teacher_id;
+        $this->category_id = $category_id;
     }
 
-    public function runDML($sql){ 
-        return $this->connection->query($sql);
+
+    public function createCourse()
+    {
+        $sql = "INSERT INTO Course (title , description , price, duration , category_id ,teacher_id, imageurl) VALUES ('" . $this->title . "' , '" . $this->description . "' , '" . $this->price . "' , '" . $this->duration . "' , '" . $this->category_id . "' , '" . $this->teacher_id . "' , '" . $this->imageurl . "');";
+        $result = $this->runDml($sql);
+        return $result;
     }
 
-    
-    public function getAllCourses(){
-        $stmt="SELECT * FROM course";
-        $result=$this->runDML($stmt); 
-        $data=$result->fetchAll(PDO::FETCH_ASSOC);
-        if($data){
-            return $data;
-        }
-        else{
-            return false;
-        }
+
+    public function getAllCourses()
+    {
+        $sql = "SELECT * FROM Course";
+        $result = $this->runDml($sql);
+        return $result->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function getoneCourses($id){
-        $stmt="SELECT * FROM course where id =$id";
-        $result=$this->runDML($stmt); 
-        $data=$result->fetchAll(PDO::FETCH_ASSOC);
-        if($data){
-            return $data;
+
+    public function updateCourse($id, $title, $description, $imageurl, $price, $duration, $category_id)
+    {
+        if (isset($imageurl) && $imageurl != '') {
+            $sql = "UPDATE Course SET title = '$title', description = '$description', price = '$price', duration = '$duration', category_id = '$category_id', imageurl = '$imageurl' WHERE id = '$id'";
+            $this->runDml($sql);
+        } else {
+            $sql = "UPDATE Course SET title = '$title', description = '$description', price = '$price', duration = '$duration', category_id = '$category_id' WHERE id = '$id'";
+            $this->runDml($sql);
         }
-        else{
-            return false;
-        }
+        return true;
     }
-    
+
+
+    public function deleteCourse($id)
+    {
+        $sql = "DELETE FROM Course WHERE id = '$id'";
+        $this->runDml($sql);
+        return true;
+    }
+
+
+    public function getCourseById($id)
+    {
+        $sql = "SELECT * FROM Course WHERE id = '$id'";
+        $result = $this->runDml($sql);
+        return $result->fetch(PDO::FETCH_ASSOC);
+    }
 }
-?>
--- src/models/Student.php
+++ src/models/Student.php
@@ -10,6 +10,10 @@ class Student extends Person
     {
         parent::__construct($first_name, $last_name, $gender, $address, $email, $password);
     }
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
 
     public function create()
     {
@@ -24,5 +28,50 @@ class Student extends Person
         $sql = "SELECT * FROM Student WHERE email = '" . $email . "' AND password = '" . $password . "';";
         $result = $this->runDml($sql);
         return $result->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getStudentById($id)
+    {
+
+        $sql = "SELECT * FROM Student WHERE student_id = '" . $id . "';";
+        $result = $this->runDml($sql);
+        return $result->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateStudent($id)
+    {
+        $sql = '';
+        if (isset($this->first_name)) {
+            $sql = "UPDATE Student SET first_name = '" . $this->first_name . "' WHERE student_id = '" . $id . "';";
+            $this->runDml($sql);
+        }
+
+        if (isset($this->last_name)) {
+            $sql = "UPDATE Student SET last_name = '" . $this->last_name . "' WHERE student_id = '" . $id . "';";
+            $this->runDml($sql);
+        }
+
+        if (isset($this->gender)) {
+            $sql = "UPDATE Student SET gender = '" . $this->gender . "' WHERE student_id = '" . $id . "';";
+            $this->runDml($sql);
+        }
+
+        if (isset($this->address)) {
+            $sql = "UPDATE Student SET address = '" . $this->address . "' WHERE student_id = '" . $id . "';";
+            $this->runDml($sql);
+        }
+
+        if (isset($this->password)) {
+            $sql = "UPDATE Student SET password = '" . $this->password . "' WHERE student_id = '" . $id . "';";
+            $this->runDml($sql);
+        }
+
+        if (isset($this->image) && $this->image != '') {
+            $sql = "UPDATE Student SET image = '" . $this->image . "' WHERE student_id = '" . $id . "';";
+            $this->runDml($sql);
+        }
+
+
+        return true;
     }
 }
